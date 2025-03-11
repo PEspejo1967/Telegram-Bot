419b4928db18eb48fd87fe008a103032aef6f762
PEspejo1967 <pespejofurniture@gmail.com>
2025-03-11 13:22:09 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -30,14 +30,17 @@ async def main():
     await client.start()  # Carga la sesión guardada
     print("✅ Bot iniciado correctamente.")
 
+    # Obtener tu información (id) para usarla en la comparación
+    me = await client.get_me()
+
     @client.on(events.NewMessage(incoming=True))
     async def handler(event):
         if event.is_private:
             sender = await event.get_sender()
             print(f"📩 Mensaje recibido de {sender.first_name}")
             
-            # Si no estás conectado, el bot responderá automáticamente con el mensaje
-            if event.sender_id != client.sender_id:  # Si el mensaje no es de ti
+            # Si el mensaje no es de ti, el bot responde automáticamente
+            if event.sender_id != me.id:  # Si el mensaje no es de ti
                 await event.respond(mensaje_auto)
             else:
                 # Si es un mensaje tuyo (estás enviando), agregar el nombre del equipo
